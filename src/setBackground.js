1bd6f8eefc5d500a4c0848afda323512020d1a6b
nrod42 <nigelrod94@gmail.com>
2022-07-03 18:58:17 -0400

    fixed image path error in getWeather function and styles.css

--- src/setBackground.js
+++ src/setBackground.js
@@ -3,9 +3,10 @@ import getTime from './getTime';
 function setBackground(response) {
   const body = document.querySelector('body');
   const hour = getTime(response);
-  if (hour >= 6 && hour <= 18) {
+
+  if (hour >= 6 && hour < 18) {
     body.style.backgroundImage = 'url(/src/img/day.jpg)';
-  } else if (hour > 18 && hour <= 20) {
+  } else if (hour >= 18 && hour < 20) {
     body.style.backgroundImage = 'url(/src/img/afternoon.jpg)';
   } else {
     body.style.backgroundImage = 'url(/src/img/night.jpg)';
