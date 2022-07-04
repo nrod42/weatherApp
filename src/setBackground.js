eb1f1024e2ac94c126cb18483b229a981b877815
Nigel <nigelrod94@gmail.com>
2022-07-04 10:21:27 -0400

    .

--- src/setBackground.js
+++ src/setBackground.js
@@ -1,15 +1,17 @@
 import getTime from './getTime';
+import './style.css';
+
 
 function setBackground(response) {
   const body = document.querySelector('body');
   const hour = getTime(response);
 
   if (hour >= 6 && hour < 18) {
-    body.style.backgroundImage = 'url(../src/img/day.jpg)';
+    body.style.backgroundImage = 'url(./src/img/day.jpg)';
   } else if (hour >= 18 && hour < 20) {
-    body.style.backgroundImage = 'url(../src/img/afternoon.jpg)';
+    body.style.backgroundImage = 'url(./src/img/afternoon.jpg)';
   } else {
-    body.style.backgroundImage = 'url(../src/img/night.jpg)';
+    body.style.backgroundImage = 'url(./src/img/night.jpg)';
   }
 }
 
